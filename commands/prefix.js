441cef33fe94bdc810a20b2c59ce2fe3c013383a
Your Name <oliver.e.rayner@gmail.com>
2022-06-25 09:49:25 -0400

    Fix for prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -18,13 +18,13 @@ module.exports = {
 
         let config = client.guildConfigs.get(interaction.guildId)
         if (!config) throw new Error('RalphError', { cause: `Config file for guild id ${interaction.guildId} does not exist. Run /config to generate one` })
-        let prefix = interaction.options._hoistedOptions[0].value
-        if (!prefix) throw new Error('RalphError', { cause: `Current prefix for guild id ${interaction.guildId} is \`${config.prefix}\`` })
+        let option = interaction.options._hoistedOptions[0]
+        if (!option) throw new Error('RalphError', { cause: `Current prefix for guild id ${interaction.guildId} is \`${config.prefix}\`` })
 
-        config.prefix = prefix
+        config.prefix = option.value
         fs.writeFileSync(`guilds/${interaction.guildId}.json`, JSON.stringify(config))
 
-        interaction.reply({ content: `:hammer: Server prefix changed to \`${prefix}\`` })
+        interaction.reply({ content: `:hammer: Server prefix changed to \`${option.prefix}\`` })
 
     }
 
